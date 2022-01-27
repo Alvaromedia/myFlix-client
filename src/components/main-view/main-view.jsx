d8daa7ff7e2936fa33d93fc99cd5762cac8a6cd0
Alvaromedia <93778428+Alvaromedia@users.noreply.github.com>
2022-01-27 21:22:36 +0100

    Update

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import axios from 'axios';
 
+import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
 export class MainView extends React.Component {
   constructor() {
     super();
+    // code executed right when the component is created in the memory
     this.state = {
       movies: [],
       selectedMovie: null,
+      user: null,
     };
   }
 
   componentDidMount() {
+    // code executed right after the component is added to the DOM
     axios
       .get('https://mxflix.herokuapp.com/movies')
       .then((response) => {
@@ -24,21 +28,36 @@ export class MainView extends React.Component {
       });
   }
 
+  // When a movie is clicked this function is invoked and updates the state of the 'selectedMovie' property to that movie
+
   setSelectedMovie(newSelectedMovie) {
     this.setState({
       selectedMovie: newSelectedMovie,
     });
   }
 
-  render() {
-    const { movies, selectedMovie } = this.state;
+  // When a user successfully logs in, this function updates the 'user' property in state to that particular user
 
+  onLoggedIn(user) {
+    this.setState({ user });
+  }
+
+  render() {
+    const { movies, selectedMovie, user } = this.state;
+
+    // If there is no user, the LoginView is rendered. If there is a user logged in, the user details are passed as a prop to the LoginView
+    if (!user) {
+      return <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />;
+    }
+
+    // Before the movies have been loaded
     if (movies.length === 0) {
       return <div className="main-view" />;
     }
 
     return (
       <div className="main-view">
+        {/* If the state of 'selectedMovie' is not null, that selected movie will be returned otherwise, all movies will be returned */}
         {selectedMovie ? (
           <MovieView
             movie={selectedMovie}
